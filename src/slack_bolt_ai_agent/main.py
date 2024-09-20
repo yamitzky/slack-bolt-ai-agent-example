b88ef52fd411a05b6a5e8ae29b2f1fd597370f6b
Mitsuki Ogasahara <negiga@gmail.com>
2024-09-20 10:42:23 +0900

    シンプルなサンプルを作成

--- src/slack_bolt_ai_agent/main.py
+++ src/slack_bolt_ai_agent/main.py
@@ -1,20 +1,17 @@
 # slack-boltのexampleコードを参考にしている: https://github.com/seratch/bolt-python/blob/assistant-apps/examples/assistants/async_interaction_app.py
 import asyncio
-import json
 import logging
 import os
-import random
 
 from dotenv import load_dotenv
 from slack_bolt.async_app import (
-    AsyncAck,
     AsyncApp,
     AsyncAssistant,
     AsyncSay,
     AsyncSetStatus,
+    AsyncSetSuggestedPrompts,
     AsyncSetTitle,
 )
-from slack_sdk.web.async_client import AsyncWebClient
 
 logging.basicConfig(level=logging.DEBUG)
 load_dotenv()
@@ -41,135 +38,23 @@ assistant = AsyncAssistant()
 # from slack_bolt.slack_sdk_assistant.thread_context_store import FileAssistantThreadContextStore
 # assistant = Assistant(thread_context_store=FileAssistantThreadContextStore())
 
+# NOTICE: もっと高度なSlackボットを作りたい人はこちらの Interaction 機能を使ったサンプルがおすすめです
+# https://github.com/slackapi/bolt-python/blob/e6ef608504258b7584909d6b1b7c9fdaeb64b2bc/examples/assistants/async_interaction_app.py
+
 
 @assistant.thread_started
-async def start_thread(say: AsyncSay):
-    await say(
-        text=":wave: 今日はどうされましたか？",
-        blocks=[
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": ":wave: 今日はどうされましたか？",
-                },
-            },
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "action_id": "assistant-generate-random-numbers",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "乱数を生成する",
-                        },
-                        "value": "1",
-                    },
-                ],
-            },
-        ],
+async def start_thread(say: AsyncSay, set_suggested_prompts: AsyncSetSuggestedPrompts):
+    await say(text=":wave: 今日はどうされましたか？")
+    await set_suggested_prompts(
+        prompts=[
+            "こんにちは",
+            # 最大４つまで指定できる
+        ]
     )
 
 
-@app.action("assistant-generate-random-numbers")
-async def configure_assistant_summarize_channel(
-    ack: AsyncAck, client: AsyncWebClient, body: dict
-):
-    await ack()
-
-    await client.views_open(
-        trigger_id=body["trigger_id"],
-        view={
-            "type": "modal",
-            "callback_id": "configure_assistant_summarize_channel",
-            "title": {"type": "plain_text", "text": "My Assistant"},
-            "submit": {"type": "plain_text", "text": "送信"},
-            "close": {"type": "plain_text", "text": "キャンセル"},
-            "private_metadata": json.dumps(
-                {
-                    "channel_id": body["channel"]["id"],
-                    "thread_ts": body["message"]["thread_ts"],
-                }
-            ),
-            "blocks": [
-                {
-                    "type": "input",
-                    "block_id": "num",
-                    "label": {"type": "plain_text", "text": "出力する乱数の数"},
-                    "element": {
-                        "type": "static_select",
-                        "action_id": "input",
-                        "placeholder": {
-                            "type": "plain_text",
-                            "text": "いくつ乱数を生成しますか？",
-                        },
-                        "options": [
-                            {"text": {"type": "plain_text", "text": "5"}, "value": "5"},
-                            {
-                                "text": {"type": "plain_text", "text": "10"},
-                                "value": "10",
-                            },
-                            {
-                                "text": {"type": "plain_text", "text": "20"},
-                                "value": "20",
-                            },
-                        ],
-                        "initial_option": {
-                            "text": {"type": "plain_text", "text": "5"},
-                            "value": "5",
-                        },
-                    },
-                }
-            ],
-        },
-    )
-
-
-@app.view("configure_assistant_summarize_channel")
-async def receive_configure_assistant_summarize_channel(
-    ack: AsyncAck, client: AsyncWebClient, payload: dict
-):
-    await ack()
-    num = payload["state"]["values"]["num"]["input"]["selected_option"]["value"]
-    thread = json.loads(payload["private_metadata"])
-    await client.chat_postMessage(
-        channel=thread["channel_id"],
-        thread_ts=thread["thread_ts"],
-        text=f"{num}個の乱数ですね。少々お待ちください！",
-        metadata={
-            "event_type": "assistant-generate-random-numbers",
-            "event_payload": {"num": int(num)},
-        },
-    )
-
-
-@assistant.bot_message
-async def respond_to_bot_messages(
-    logger: logging.Logger, set_status: AsyncSetStatus, say: AsyncSay, payload: dict
-):
-    try:
-        if (
-            payload.get("metadata", {}).get("event_type")
-            == "assistant-generate-random-numbers"
-        ):
-            await set_status("ランダムな数字のリストを生成しています...")
-            await asyncio.sleep(1)
-            nums: set[str] = set()
-            num = payload["metadata"]["event_payload"]["num"]
-            while len(nums) < num:
-                nums.add(str(random.randint(1, 100)))
-            await say(f"こちらです: {', '.join(nums)}")
-        else:
-            # nothing to do for this bot message
-            # If you want to add more patterns here, be careful not to cause infinite loop messaging
-            pass
-
-    except Exception as e:
-        logger.exception(f"Failed to respond to an inquiry: {e}")
-
-
-@assistant.user_message(matchers=[lambda body: "help page" in body["event"]["text"]])  # type: ignore
+# キーワードに引っ掛けて処理できる
+@assistant.user_message(matchers=[lambda body: "こんにちは" in body["event"]["text"]])  # type: ignore
 async def find_help_pages(
     payload: dict,
     logger: logging.Logger,
@@ -181,9 +66,7 @@ async def find_help_pages(
         await set_title(payload["text"])
         await set_status("ヘルプページを検索中...")
         await asyncio.sleep(0.5)
-        await say(
-            "こちらのヘルプページをご確認ください: https://www.example.com/help-page-123"
-        )
+        await say("こんにちは！こんにちは！")
     except Exception as e:
         logger.exception(f"Failed to respond to an inquiry: {e}")
         await say(f":warning: リクエストの処理中にエラーが発生しました (エラー: {e})")
